2bb7f6a26630e753143d418d4ecb1625be6d7771
Stephanie Burlinchon <stephanieb170@gmail.com>
2023-03-27 22:42:38 +0000

    .js add to list and save

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
-
 const newTask = document.getElementById('newtask');
 const listToDo = document.getElementById('list');
 
-let list;
+let list = localStorage.getItem('list');
 
-  document.getElementById('add').onclick = function() {
-    list = newTask.value;
-    listToDo.innerHTML += list;
-  }
+if (list) {
+  listToDo.innerHTML = list;
+}
 
+document.getElementById('add').setAttribute('type', 'button');
 
+document.getElementById('add').onclick = function() {
+  const listItem = document.createElement('li');
+  listItem.innerText = newTask.value;
+  listToDo.insertBefore(listItem, listToDo.childNodes[0]);
+  newTask.value = '';
+  
+  localStorage.setItem('list', listToDo.innerHTML);
+}
