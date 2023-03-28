a45ab8a1fc2fb02562eae5c806779425ad43776f
Stephanie Burlinchon <stephanieb170@gmail.com>
2023-03-28 00:08:11 +0000

    add, remove and edit list items

--- script.js
+++ script.js
@@ -11,7 +11,29 @@ document.getElementById('add').setAttribute('type', 'button');
 
 document.getElementById('add').onclick = function() {
   const listItem = document.createElement('li');
+  const edit = document.createElement('img');
+  edit.setAttribute('src', 'images/edit.svg');
+  edit.classList.add('edit-icon');
+  edit.addEventListener('click', function() {
+    const editText = prompt('Enter updated text:');
+    if (editText) {
+      listItem.firstChild.textContent = editText;
+      localStorage.setItem('list', listToDo.innerHTML);
+    }
+  });
+  
+  const trash = document.createElement('img');
+  trash.setAttribute('src', 'images/trash.svg');
+  trash.classList.add('trash-icon');
+  trash.addEventListener('click', function() {
+    listItem.remove();
+    localStorage.setItem('list', listToDo.innerHTML);
+  });
+
   listItem.innerText = newTask.value;
+  listItem.appendChild(edit);
+  listItem.appendChild(trash);
+  
   listToDo.insertBefore(listItem, listToDo.childNodes[0]);
   newTask.value = '';
   
